d647499c4076388556d8cd79684c17d6d2bda587
Arne Brasseur <arne@arnebrasseur.net>
2019-01-23 14:26:59 +0800

    Make emybar roughly MELPA compliant

--- emybar/emybar.el
+++ emybar/emybar.el
@@ -1,3 +1,38 @@
+;;; emybar.el --- -*- lexical-binding: t; -*-
+
+;; Filename: emybar.el
+;; Author: Arne Brasseur <arne@arnebrasseur.net>
+;; Maintainer: Arne Brasseur <arne@arnebrasseur.net>
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Commentary:
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Change Log:
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; Copyright (C) 2019  Arne Brasseur
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or (at
+;; your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <https://www.gnu.org/licenses/>.
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Code:
+
 (setq emybar-colors '())
 (setq emybar-procs '())
 
@@ -55,3 +90,8 @@ server to listen for color updates."
 
 ;; (pop mode-line-format)
 ;; (mapcar #'delete-process (a-vals emybar-procs))
+
+(provide 'emybar)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; emybar.el ends here
